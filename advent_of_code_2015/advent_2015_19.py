382767bfd4854427911b82d55ca0c270046b2444
Elena Tishkovskaya <elidare@users.noreply.github.com>
2024-03-07 00:21:21 +0300

    Add Day 19 Final version (not a solution tho)

--- advent_of_code_2015/advent_2015_19.py
+++ advent_of_code_2015/advent_2015_19.py
@@ -1,5 +1,13 @@
 # Part 1 works
-# Part 2 does not. I'll skip it and look at it later at reddit
+# Part 2 does not work.
+# I still haven't made it, I've tried reading reddit explanations, but with no success really
+# Taken several solutions to get a star
+# because I don't like unfinished business
+
+# !! https://www.reddit.com/r/adventofcode/comments/3xflz8/comment/cy4etju/
+
+from collections import deque
+
 
 with open('19.txt', 'r') as f:
     lines = f.readlines()
@@ -21,90 +29,43 @@ def part_one():
     return len(set(distinct_molecules))
 
 
-def part_two_2():
-    queue = [[formula]]  # Let's keep all the steps in a list and make a queue out of them
-    min_steps = 1_000_000_000  # Some big value
-
-    while queue:
-        print('Queue length: ', len(queue))
-        # print('queue____', queue)
-        formulas = queue.pop()
-        molecule = formulas[-1]
-        print(molecule, len(formulas))
-
-        if molecule == 'e':
-            steps = len(formulas) - 1
-            print('Minimum steps: ', steps)
-            min_steps = min(steps, min_steps)
-            continue
-
-        for r in replacements_sorted:
-            original, replacement = r
-            index = molecule.find(replacement)
-
-            while index > -1:
-                # print('Inside while', index)
-                print('FIND: ', replacement, original)
-                new_molecule = molecule[:index] + original + molecule[index + len(replacement):]
-                if new_molecule != molecule and new_molecule not in formulas:
-                    formulas_copy = formulas[:]
-                    formulas_copy.append(new_molecule)
-                    print('______________ new_molecule', new_molecule)
-                    queue.append(formulas_copy)
-
-                index = molecule.find(replacement, index + 1)
-
-    return min_steps
-
-
 def part_two():
-    queue = [(formula, 0)]
+    # NOT WORKING CORRECTLY
+    queue = deque([(formula, 0)])
     min_steps = 1_000_000_000  # Some big value
-    formulas_steps = dict()
-    formulas_steps[formula] = 0
-    dead_end = list()
 
     replacements_sorted = sorted(replacements, key=lambda x: len(x[1]), reverse=True)
 
     while queue:
-        print('Queue length: ', len(queue))
         current_molecule, current_steps = queue.pop()
-        # print(current_molecule, current_steps)
-        if (current_molecule, current_steps) in dead_end:
-            continue
 
         if current_molecule == 'e':
             print('Minimum steps: ', current_steps)
             min_steps = min(current_steps, min_steps)
             continue
 
-        found = False
         for r in replacements_sorted:
             original, replacement = r
             index = current_molecule.find(replacement)
 
             while index > -1:
-                found = True
                 new_molecule = current_molecule[:index] + original + current_molecule[index + len(replacement):]
-
-                # if formulas_steps.get(new_molecule) and formulas_steps.get(new_molecule) <= current_steps + 1 \
-                #         or new_molecule == current_molecule:
-                #     continue
-                # if new_molecule != current_molecule:
-                print('new_molecule', new_molecule)
-                # formulas_steps[new_molecule] = current_steps + 1
                 if (new_molecule, current_steps + 1) not in queue:
                     queue.append((new_molecule, current_steps + 1))
                 index = current_molecule.find(replacement, index + 1)
-        else:
-            # fixme how to mark a dead-end molecule
-            if not found:
-                # print('NOT FOUND_____', current_molecule, current_steps)
-                dead_end.append((current_molecule, current_steps))
 
     return min_steps
 
 
+def extra(basis):
+    # https://www.reddit.com/r/adventofcode/comments/19eb08o/comment/kjdqeaw/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
+    # How the actual f__k?!
+    rn = basis.count('Rn')
+    ar = basis.count('Ar')
+    yy = basis.count('Y')
+    ele = sum(c < 'a' for c in basis)
+    print("Part 2:", ele - rn - ar - yy - yy - 1)
 
-# print(part_one())
+
+print(part_one())
 print(part_two())
