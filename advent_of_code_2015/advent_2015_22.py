152b2feaa600a3b0aa863279874c4666d863321e
Elena Tishkovskaya <elidare@users.noreply.github.com>
2024-03-10 23:33:09 +0300

    Add Day 22 Part 1

--- advent_of_code_2015/advent_2015_22.py
+++ advent_of_code_2015/advent_2015_22.py
@@ -1,8 +1,12 @@
-from collections import deque
-
-# https://pastebin.com/u85hecvv
+# That took about 4 or 5 days, but thanks to this reddit comment
 # https://www.reddit.com/r/adventofcode/comments/162ntk9/2015_day_22_part_2_golang_stuck_on_part_2_value/
-# https://github.com/tomribbens/advent-of-code-tribbe
+# and the user Interesting_Fly_3396's golang solution https://pastebin.com/u85hecvv
+# I eventually understood how I should solve it.
+# Which I did! Wooohooo!
+# TODO fix Part 2
+
+from collections import deque
+import copy
 
 
 class Player:
@@ -18,99 +22,156 @@ class Player:
         damage -= self.armor
         damage = damage if damage > 0 else 1
         self.hp -= damage
-        print('boss had', damage)
-
-    # def get_current_stats(self):
-    #     return self.damage, self.armor
+        # print(f'Boss has hit with {damage}')
 
 
 class Wizard(Player):
     def __init__(self, damage=0, armor=0, hp=50, mana=500):
         super().__init__(damage, armor, hp)
         self.mana = mana
-
-    # def get_current_stats(self):
-    #     damage = 0
-    #     armor = self.armor
-        """
-        Magic Missile costs 53 mana. It instantly does 4 damage.
-    Drain costs 73 mana. It instantly does 2 damage and heals you for 2 hit points.
-    Shield costs 113 mana. It starts an effect that lasts for 6 turns. While it is active, your armor is increased by 7.
-    Poison costs 173 mana. It starts an effect that lasts for 6 turns. At the start of each turn while it is active, it deals the boss 3 damage.
-    Recharge costs 229 mana. It starts an effect that lasts for 5 turns. At the start of each turn while it is active, it gives you 101 new mana.
-        """
-        # if self.spells['Shield'].is_active():
-        #     armor += 7
-        #     self.spells['Shield'].decrement()
-        # if self.spells['Poison'].is_active():
-        #     damage += 3
-        #     self.spells['Poison'].decrement()
-        # if self.spells['Recharge'].is_active():
-        #     self.mana += 101
-        #     self.spells['Recharge'].decrement()
-        #
-        # return damage, armor
+        self.mana_used = 0
 
     def set_damage_through_armor(self, damage, armor):
         damage -= armor
         damage = damage if damage > 0 else 1
         self.hp -= damage
 
-    # def cast_and_get_instant_damage(self, spell):  # cast_and_get_instant_damage
-    #     print('CAST:', spell)
-    #     damage = 0
-    #     if self.spells[spell].mana_cost <= self.mana:
-    #         self.mana -= self.spells[spell].activate()
-    #         if spell == 'MagicMissile':
-    #             damage = 4
-    #         elif spell == 'Drain':
-    #             damage = 2
-    #             self.hp += 2
-    #     else:
-    #         print('Not enough mana')
-    #     return damage
-
 
 class Spell:
     def __init__(self, mana_cost, lasts=0):
-        self.active = False
         self.timer = 0
         self.lasts = lasts
         self.mana_cost = mana_cost
 
     def decrement(self):
-        if self.timer > 0:
+        if self.is_active():
             self.timer -= 1
-        else:
-            self.active = False
 
     def activate(self):
-        self.active = True
         self.timer = self.lasts
         return self.mana_cost
 
     def is_active(self):
-        return self.active
+        return self.timer > 0
 
 
-def apply_effects():
-    for sn, s in spells.items():
-        match sn:
-            case 'Shield':
-                wizard.armor = 7 if s.is_active() else 0
-            case 'Poison':
-                if s.is_active():
-                    boss.hp -= 3
-            case 'Recharge':
-                if s.is_active():
-                    wizard.mana += 101
+class Game:
+    def __init__(self, wizard, boss, spells, hard_level=False):
+        self.wizard = wizard
+        self.boss = boss
+        self.spells = spells
+        self.spell_chain = list()
+        self.wizard_win = None
+        self.boss_win = None
+        self.hard_level = hard_level
 
-        s.decrement()
+    def apply_wizard_turn(self):
+        # print('_____wizard turn')
+        if self.hard_level:
+            self.wizard.hp -= 1
+            if not self.wizard.is_alive():
+                print('Wizard is dead by hp loss')
+                self.boss_win = True
+                return
 
+        current_spell = self.spell_chain[-1]
+        # get wizard stats
+        # print(f'wizard damage {self.wizard.damage}, wizard armor {self.wizard.armor}, wizard hp {self.wizard.hp}, mana {self.wizard.mana}')
+        # get boss stats
+        # print(f'boss_damage {self.boss.damage}, boss_armor {self.boss.armor}, boss hp {self.boss.hp}')
 
-wizard = Wizard(hp=10, mana=250)
-# Do not commit the real input
-boss = Player(damage=8, hp=13)
+        # apply lasting effects if any
+        self.apply_effects()
+
+        if not self.boss.is_alive():
+            print('Boss is dead! Mana used', self.wizard.mana_used)
+            self.wizard_win = True
+            return
+
+        # cast a spell
+        # print('Wizards turn, casting', current_spell)
+        self.wizard.mana_used += self.cast(current_spell)
+
+        if not self.boss.is_alive():
+            print('Boss dies, Player wins! Mana used', self.wizard.mana_used)
+            self.wizard_win = True
+
+    def apply_boss_turn(self):
+        # print('_______Bosses turn')
+        if self.hard_level:
+            self.wizard.hp -= 1
+            if not self.wizard.is_alive():
+                print('Wizard is dead by hp loss')
+                self.boss_win = True
+                return
+
+        # get wizard stats
+        # print(f'wizard damage {self.wizard.damage}, wizard armor {self.wizard.armor}, wizard hp {self.wizard.hp}, mana {self.wizard.mana}')
+        # get boss stats
+        # print(f'boss_damage {self.boss.damage}, boss_armor {self.boss.armor}, boss hp {self.boss.hp}')
+
+        # apply lasting effects if any
+        self.apply_effects()
+
+        if not self.boss.is_alive():
+            print('Boss is dead! Mana used', self.wizard.mana_used)
+            self.wizard_win = True
+            return
+
+        # boss attacks the wizard
+        # print(f'Boss attacks with damage {self.boss.damage}, hits {self.boss.damage - self.wizard.armor}')
+        self.wizard.set_damage_through_armor(self.boss.damage, self.wizard.armor)
+
+        if not self.wizard.is_alive():
+            # print('Wizard dies :(')
+            self.boss_win = True
+
+    def apply_effects(self):
+        for sn, s in self.spells.items():
+            match sn:
+                case 'Shield':
+                    self.wizard.armor = 7 if s.is_active() else 0
+                case 'Poison':
+                    if s.is_active():
+                        self.boss.set_damage(3)
+                case 'Recharge':
+                    if s.is_active():
+                        self.wizard.mana += 101
+
+            s.decrement()
+
+    def cast(self, spell):
+        mana_cost = self.spells[spell].activate()
+        self.wizard.mana -= mana_cost
+
+        match spell:
+            case 'MagicMissile':
+                self.boss.set_damage(4)
+            case 'Drain':
+                self.boss.set_damage(2)
+                self.wizard.hp += 2
+
+        return mana_cost
+
+    def next_spell(self, spell):
+        # print('Append next spell to', self, self.spell_chain, spell)
+        self.spell_chain.append(spell)
+
+    def get_possible_spells(self):
+        # print('Checking possible spells with mana', self.wizard.mana)
+        possible_spells = list()
+        for sn, s in self.spells.items():
+            if not s.is_active() and s.mana_cost <= self.wizard.mana:
+                possible_spells.append(sn)
+
+        return sorted(possible_spells, key=lambda x: spells[x].mana_cost)
+
+    def is_game_ended(self):
+        return self.wizard_win or self.boss_win
+
+    def get_mana_used(self):
+        return self.wizard.mana_used
+
 
 spells = {
     'MagicMissile': Spell(53),
@@ -119,84 +180,57 @@ spells = {
     'Poison': Spell(173, 6),
     'Recharge': Spell(229, 5),
 }
-total_mana_cost = 0
 
 
-def cast(spell):
-    # check current mana whether it is enough
-    # check (before) if this spell is not active atm
-    mana_cost = spells[spell].activate()
-    wizard.mana -= mana_cost
+def play(level_hard=False):
+    minimum_mana_cost = initial_mana_cost = 1_000_000
+    wizard = Wizard(hp=50, mana=500)
+    # Do not commit the real boss input
+    boss = Player(damage=8, hp=13)  # todo
+    game = Game(wizard, boss, copy.deepcopy(spells), level_hard)
 
-    match spell:
-        case 'MagicMissile':
-            boss.hp -= 4
-        case 'Drain':
-            boss.hp -= 2
-            wizard.hp += 2
+    queue = deque([game])
 
-    return mana_cost
+    while queue:
+        current_game = queue.popleft()
 
+        # Choose next spell to cast
+        next_spells = current_game.get_possible_spells()
 
-def apply_wizard_turn():
-    print('_____wizard turn')
-    # apply lasting effects if any
-    apply_effects()
-    # get wizard stats
-    wizard_damage, wizard_armor = wizard.damage, wizard.armor
-    print('wizard_damage, wizard_armor', wizard_damage, wizard_armor, 'wizard hp', wizard.hp)
-    # get boss stats
-    boss_damage, boss_armor = boss.damage, boss.armor
-    print('boss_damage, boss_armor', boss_damage, boss_armor, 'boss hp', boss.hp)
+        if not len(next_spells):
+            continue
 
-    # cast a spell
-    total_mana_cost += cast('Drain')
-    # add up the mana used
-    # check if the boss is dead
+        for sp in next_spells:
+            new_game = copy.deepcopy(current_game)
+            new_game.next_spell(sp)
 
-    # print('Wizards turn, casting', spell)
-    # wizard_damage += wizard.cast_and_get_instant_damage(spell)
-    # print('wizard_damage after cast', wizard_damage)
-    # boss.set_hit(wizard_damage)
+            # Wizard's turn
+            new_game.apply_wizard_turn()
+            # Check if any player is dead
+            if new_game.is_game_ended():
+                if new_game.wizard_win:
+                    minimum_mana_cost = min(minimum_mana_cost, new_game.get_mana_used())
+                continue
 
+            # Boss's turn
+            new_game.apply_boss_turn()
+            # Check if any player is dead
+            if new_game.is_game_ended():
+                if new_game.wizard_win:
+                    minimum_mana_cost = min(minimum_mana_cost, new_game.get_mana_used())
+                continue
 
-def apply_boss_turn():
-    print('_______Bosses turn')
-    # apply lasting effects if any
-    apply_effects()
-    # get wizard stats
-    wizard_damage, wizard_armor = wizard.damage, wizard.armor
-    print('wizard_damage, wizard_armor', wizard_damage, wizard_armor, 'wizard hp', wizard.hp)
-    # get boss stats
-    boss_damage, boss_armor = boss.damage, boss.armor
-    print('boss_damage, boss_armor', boss_damage, boss_armor, 'boss hp', boss.hp)
+            queue.append(new_game)
 
-    # attack the wizard
-    # check if the wizard is dead
-    # boss's turn
-    # wizard.set_hit_through_armor(boss_damage, wizard_armor)
-
-
-def get_game_state():
-    return f'Wizard: hp {wizard.hp}, mana {wizard.mana}, Boss: {boss.hp}'
-
-
-def play():
-
-    turn = 1
-    queue = deque(['Poison', 'MagicMissile'])
-    while True:
-        if turn % 2 != 0:
-            apply_wizard_turn()
-        else:
-            apply_boss_turn()
-
-        turn += 1
-
-        if turn == 4:
+        # I sort the possible spells by their mana cost, ascending,
+        # so I believe once I have found a situation where the Wizard defeats the Boss
+        # this is a situation with minimum mana.
+        # That reduces the script time vastly
+        if minimum_mana_cost < initial_mana_cost:
             break
 
-    return 'Not ready'
+    return f'Minimum mana cost: {minimum_mana_cost}'
 
 
-print(play())
+# print(play())
+print(play(True))  # hard level
