0b6ee0c7da07ed9566ff32df0e6fba1bf44f8e5e
elidare <elidare.jjj@gmail.com>
2025-11-04 17:56:35 +0200

    Add addresses to inserts

--- holistic_project/algo_drafts/mock_data/get_inserts.py
+++ holistic_project/algo_drafts/mock_data/get_inserts.py
@@ -7,27 +7,30 @@ restaurant_values = []
 with open('stations_restaurants_fixed_cuisines.json', 'r', encoding='utf-8') as stations:
     stations = json.load(stations)
     for st in stations:
-        if 'Espoo' in st['address'] or 'Helsinki' in st['address']:  # TEMP for testing, delete this if for all the data
-            station_values.append(f"('{st['name']}', ST_GeogFromText('SRID=4326;POINT({st['lon']} {st['lat']})'))")
+        station_values.append(f"('{st['name']}', "
+                              f"ST_GeogFromText('SRID=4326;POINT({st['lon']} {st['lat']})'), "
+                              f"'{st['address']}')")
 
-            for r in st['restaurants']:
-                restaurant_name = r['name'].replace("'", "''")
-                restaurant_values.append(f"('{st['name']}', '{restaurant_name}', "
-                                         f"ST_GeogFromText('SRID=4326;POINT({r['lon']} {st['lat']})'), "
-                                         f"ARRAY[{', '.join(repr(c) for c in r['cuisine'])}])")
+        for r in st['restaurants']:
+            restaurant_name = r['name'].replace("'", "''")
+            restaurant_address = st['address'] if r['address'] == "Not provided" else r['address']
+            restaurant_values.append(f"('{st['name']}', '{restaurant_name}', "
+                                     f"ST_GeogFromText('SRID=4326;POINT({r['lon']} {st['lat']})'), "
+                                     f"ARRAY[{', '.join(repr(c) for c in r['cuisine'])}], "
+                                     f"'{restaurant_address}')")
 
 with open('inserts', 'a', encoding='utf-8') as file:
     station_insert = f"""
-INSERT INTO stations (name, location)
+INSERT INTO stations (name, location, address)
 VALUES {',\n'.join(station_values)};"""
 
     restaurant_insert = f"""
-INSERT INTO restaurants (station_id, name, location, cuisines)
-SELECT s.station_id, d.name, d.location, d.cuisines
+INSERT INTO restaurants (station_id, name, location, cuisines, address)
+SELECT s.station_id, d.name, d.location, d.cuisines, d.address
 FROM (
   VALUES
     {',\n'.join(restaurant_values)}
-) AS d(station_name, name, location, cuisines)
+) AS d(station_name, name, location, cuisines, address)
 JOIN stations AS s
 ON s.name = d.station_name;"""
 
